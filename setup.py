bd1c1bb9869d4117a335db7162faf4e471497ab9
davidchen0420 <57333478+davidchen0420@users.noreply.github.com>
2024-04-15 21:21:01 -0400

    tutorial

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='IsoMarker',
-    version='0.1.0',    
+    version='0.2.0',    
     description='IsoMarker',
     url='https://github.com/davidchen0420',
     author='David Chen',
